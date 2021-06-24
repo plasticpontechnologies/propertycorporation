78f534a893c7d34a05d2572b8cf4fdc1a2d63eb7
JANAKI RAM <JANAKI RAM@DESKTOP-95MHJG6>
2021-06-24 12:21:39 +0530

    first commit

---
+++ propertycorporation/src/main/java/com/plasticon/propertycorporation/PropertycorporationApplication.java
@@ -0,0 +1,13 @@
+package com.plasticon.propertycorporation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertycorporationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertycorporationApplication.class, args);
+	}
+
+}
---
+++ propertycorporation/src/test/java/com/plasticon/propertycorporation/PropertycorporationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plasticon.propertycorporation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertycorporationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
